290424d2d146c3bcb0155b593670bee56be91ac9
Abdoulaye Barry <alexbarrykeriwel123@gmail.com>
2026-06-27 17:52:08 +0000

    Edit server.js

--- api/server.js
+++ api/server.js
@@ -26,6 +26,6 @@ app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server listening on the port::${port}`);
 });
